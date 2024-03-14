e37f6379e9522165eaa3021119e25f457b7b96a5
Eugene <cullofdute19762005@gmail.com>
2024-03-15 00:07:37 +0300

    initial commit

---
+++ make_order/make_order/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите название товара: ");
+var productName = Console.ReadLine();
+
+Console.Write("Введите количество товара: ");
+var productQuantityInput = Console.ReadLine();
+int productQuantity = int.Parse(productQuantityInput);
+
+Console.Write("Введите свое имя: ");
+var personName = Console.ReadLine();
+
+Console.Write("Введите адрес доставки: ");
+var address = Console.ReadLine();
+
+DateTime date = DateTime.Now;
+date.AddDays(3);
+string futureDate = date.ToString("d");
+
+Console.Clear();
+
+Console.WriteLine($"Здравствуйте, {personName}, вы заказали {productQuantity} {productName} на адрес {address}, все верно? (y/n)");
+var answer = Console.ReadLine();
+
+Console.Clear();
+
+if (answer == "y")
+{
+    Console.WriteLine($"{personName}!\nВаш заказ {productName} в количестве {productQuantity} оформлен!\n" +
+        $"Ожидайте доставку по адресу {address} к {futureDate}");
+}
+else
+{
+    Console.WriteLine("Перезаполните форму. Всего хорошего!");
+}
