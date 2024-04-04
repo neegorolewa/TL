98f9af5a441defb52eaf024f3d7588549bc5669e
Eugene <cullofdute19762005@gmail.com>
2024-04-04 16:16:26 +0300

    исправил ошибки в issues, готово к проверке

--- homework1/dictionary/dictionary/dictionary.cs
+++ homework1/dictionary/dictionary/dictionary.cs
@@ -1,211 +1,233 @@
-﻿const string menu = "Выберите действие: \n" +
-        $"(1) Добавить перевод\n" +
-        $"(2) Удалить перевод\n" +
-        $"(3) Изменить перевод\n" +
-        $"(4) Перевести слово\n" +
-        $"(5) Выйти";
+﻿namespace Dictionary;
 
-string dictFilePath = "dict.txt";
-
-Dictionary<string, string> dictionary = new Dictionary<string, string>();
-
-void Wait()
+class Program
 {
-    Console.WriteLine("Введите любую клавишу для продолжения...");
-    Console.ReadKey();
-    Console.Clear();
-}
+    const string MENU = "Выберите действие: \n" +
+            $"(1) Добавить перевод\n" +
+            $"(2) Удалить перевод\n" +
+            $"(3) Изменить перевод\n" +
+            $"(4) Перевести слово\n" +
+            $"(5) Выйти";
 
-void AddTranslate()
-{
-    Console.Write("Введите слово: ");
-    var word = Console.ReadLine();
+    string dictFilePath = "dict.txt";
 
-    Console.Write("Введите перевод: ");
-    var translate = Console.ReadLine();
+    Dictionary<string, string> dictionary = new Dictionary<string, string>();
 
-    if (word == "" || translate == "")
+    static void Main()
     {
-        Console.WriteLine("Вы не ввели значение (пустая строка)");
-        Wait();
-    }
-    else
-    {
-        translate.ToString();
-        word.ToString();
-
-        dictionary[word] = translate;
-
-        Wait();
-    }
-    
-}
-
-void DeleteTranslate()
-{
-    Console.Write("Введите слово, которое хотите удалить: ");
-    var word = Console.ReadLine();
-    if (word == "")
-    {
-        Console.WriteLine("Вы не ввели значение (пустая строка)");
-        Wait();
-    }
-    else
-    {
-        word.ToString();
-
-        if (dictionary.ContainsKey(word))
+        Program program = new Program();
+        program.LoadDictionaryFromFile();
+        while (true)
         {
-            dictionary.Remove(word);
-            Console.WriteLine($"Перевод слова '{word}' удален из словаря");
+            Console.WriteLine(MENU);
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Clear();
+                    program.AddTranslate();
+                    break;
+                case "2":
+                    Console.Clear();
+                    program.DeleteTranslate();
+                    break;
+                case "3":
+                    Console.Clear();
+                    program.ChangeTranslate();
+                    break;
+                case "4":
+                    Console.Clear();
+                    program.TranslateWord();
+                    break;
+                case "5":
+                    Console.Clear();
+                    Console.Write("Хотите сохранить изменения в словаре? Введите y для сохранения перед выходом.: \n");
+                    var saveChanges = Console.ReadLine();
+                    if (saveChanges == "y")
+                    {
+                        program.SaveDictionary();
+                        Console.WriteLine("Изменения успешно сохранены");
+                    }
+                    return;
+                default:
+                    Console.WriteLine("Неверно введено действие...");
+                    continue;
+            }
         }
-        else
-        {
-            Console.WriteLine($"Слово '{word}' не найдено в словаре");
-        }
-
-        Wait();
     }
-}
 
-void ChangeTranslate()
-{
-    Console.Write("Введите слово, перевод которого хотите изменить: ");
-    var word = Console.ReadLine();
-
-    if (word == "")
+    void AddTranslate()
     {
-        Console.WriteLine("Вы не ввели значение (пустая строка)");
-        Wait();
-    }
-    else
-    {
-        word.ToString();
-
-        if (dictionary.ContainsKey(word))
+        try
         {
-            Console.Write("Введите новый перевод: ");
+            Console.Write("Введите слово: ");
+            var word = Console.ReadLine();
+
+            Console.Write("Введите перевод: ");
             var translate = Console.ReadLine();
-            if (translate == "")
+
+            if (word == "" || translate == "")
             {
                 Console.WriteLine("Вы не ввели значение (пустая строка)");
-                Wait();
             }
             else
             {
                 dictionary[word] = translate;
-                Console.WriteLine($"Перевод слова '{word}' изменен на '{translate}'");
-                Wait();
             }
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine($"Слово '{word}' не найдено в словаре");
-            Wait();
-        }
-    }
-}
-
-void TranslateWord()
-{
-    Console.Write("Введите слово, которое хотите перевести: ");
-    var word = Console.ReadLine();
-    if (word == "")
-    {
-        Console.WriteLine("Вы не ввели значение (пустая строка)");
-        Wait();
-    }
-    else
-    {
-        word.ToString();
-
-        if (dictionary.ContainsKey(word))
-        {
-            Console.WriteLine($"Перевод слова '{word}' - {dictionary[word]}");
-            Wait();
-        }
-        else
-        {
-            Console.WriteLine($"Слово '{word}' не найдено в словаре");
-            Wait();
-        }
-    }
-}
-
-async void SaveDictionary()
-{
-    using (StreamWriter file = new StreamWriter(dictFilePath, false))
-    {
-        foreach(var pair in dictionary)
-        {
-            await file.WriteLineAsync($"{pair.Key} {pair.Value}");
+            Console.WriteLine($"Произошла ошибка при удалении перевода: {ex.Message}");
+            throw;
         }
 
     }
-}
 
-try
-{
-    using (StreamReader inFile = new StreamReader(dictFilePath))
+    void DeleteTranslate()
     {
-        string? line;
-        while ((line = await inFile.ReadLineAsync()) != null)
+        try
         {
-            string[] parts = line.Split();
-            if (parts.Length == 2)
+            Console.Write("Введите слово, которое хотите удалить: ");
+            var word = Console.ReadLine();
+            if (word == "")
             {
-                string word = parts[0].Trim();
-                string translate = parts[1].Trim();
-                dictionary[word] = translate;
+                Console.WriteLine("Вы не ввели значение (пустая строка)");
+            }
+            else
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary.Remove(word);
+                    Console.WriteLine($"Перевод слова '{word}' удален из словаря");
+                }
+                else
+                {
+                    Console.WriteLine($"Слово '{word}' не найдено в словаре");
+                }
             }
         }
-    }
-}
-catch
-{
-    Console.WriteLine("Ошибка считывания файла");
-}
-
-while (true)
-{
-    Console.WriteLine(menu);
-    var choice = Console.ReadLine();
-
-    if (choice == "")
-    {
-        Console.WriteLine("Введите номер действия...");
-        Wait();
-        continue;
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка при удалении перевода: {ex.Message}");
+            throw;
+        }
     }
 
-    choice.ToString();
-
-    switch (choice)
+    void ChangeTranslate()
     {
-        case "1":
-            Console.Clear();
-            AddTranslate();
-            break;
-        case "2":
-            Console.Clear();
-            DeleteTranslate();
-            break;
-        case "3":
-            Console.Clear();
-            ChangeTranslate();
-            break;
-        case "4":
-            Console.Clear();
-            TranslateWord();
-            break;
-        case "5":
-            Console.Clear();
-            Console.Write("Хотите сохранить изменения в словаре? Введите y для сохранения перед выходом.: \n");
-            var saveChanges = Console.ReadLine();
-            if (saveChanges == "y")
+        try
+        {
+            Console.Write("Введите слово, перевод которого хотите изменить: ");
+            var word = Console.ReadLine();
+
+            if (word == "")
             {
-                SaveDictionary();
+                Console.WriteLine("Вы не ввели значение (пустая строка)");
             }
-            return 0;
+            else
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    Console.Write("Введите новый перевод: ");
+                    var translate = Console.ReadLine();
+                    if (translate == "")
+                    {
+                        Console.WriteLine("Вы не ввели значение (пустая строка)");
+                    }
+                    else
+                    {
+                        dictionary[word] = translate;
+                        Console.WriteLine($"Перевод слова '{word}' изменен на '{translate}'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Слово '{word}' не найдено в словаре");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка при изменении перевода: {ex.Message}");
+            throw;
+        }
+    }
+
+    void TranslateWord()
+    {
+        try
+        {
+            Console.Write("Введите слово, которое хотите перевести: ");
+            var word = Console.ReadLine();
+            if (word == "")
+            {
+                Console.WriteLine("Вы не ввели значение (пустая строка)");
+            }
+            else
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    Console.WriteLine($"Перевод слова '{word}' - {dictionary[word]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Слово '{word}' не найдено в словаре");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка при переводе слова: {ex.Message}");
+            throw;
+        }
+
+    }
+
+    async void SaveDictionary()
+    {
+        try
+        {
+            using (StreamWriter dictFile = new StreamWriter(dictFilePath, false))
+            {
+                foreach (var pair in dictionary)
+                {
+                    await dictFile.WriteLineAsync($"{pair.Key} {pair.Value}");
+                }
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка при cохранении изменений: {ex.Message}");
+            throw;
+        }
+
+    }
+
+    async void LoadDictionaryFromFile()
+    {
+        try
+        {
+            using (StreamReader dictFile = new StreamReader(dictFilePath))
+            {
+                string? line;
+                while ((line = await dictFile.ReadLineAsync()) != null)
+                {
+                    string[] wordWithTranslation = line.Split();
+                    if (wordWithTranslation.Length == 2)
+                    {
+                        string word = wordWithTranslation[0].Trim();
+                        string translate = wordWithTranslation[1].Trim();
+                        dictionary[word] = translate;
+                    }
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка считывания файла");
+            throw;
+        }
     }
 }
